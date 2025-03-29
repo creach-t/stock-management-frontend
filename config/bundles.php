f0d68607bd9952cac87ed18f749e766a3d935c0f
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-03-29 13:27:56 +0100

    Correction du fichier bundles.php en commentant MonologBundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    // Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true], // Temporarily commented out
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
